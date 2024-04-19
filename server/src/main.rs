d5c299481728ba442a2a62691d2000c0bd13bd33
Søren Alsbjerg Hørup <sah@sah.dk>
2024-04-19 14:18:45 +0200

    added todo

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,15 @@
 use std::collections::HashMap;
 
 use endlessgrid::Grid;
+use netcode::server::Server;
 use slotmap::{DefaultKey, SlotMap};
 use tiled::Loader;
 
 #[derive(Clone, Default)]
-pub struct Tile {
-    
-}
-pub struct Entity {
+pub struct Tile {}
+pub struct Entity {}
 
-}
-
-fn load_map(grid:&mut Grid<Tile>, _entities:&mut SlotMap<DefaultKey, Entity>) {
+fn load_map(grid: &mut Grid<Tile>, _entities: &mut SlotMap<DefaultKey, Entity>) {
     let mut loader = Loader::new();
     let map = loader.load_tmx_map("assets/maps/basic.tmx").unwrap();
     for layer in map.layers() {
@@ -30,24 +27,32 @@ fn load_map(grid:&mut Grid<Tile>, _entities:&mut SlotMap<DefaultKey, Entity>) {
                             chunk_pos.0 * tiled::ChunkData::WIDTH as i32 + x,
                             chunk_pos.1 * tiled::ChunkData::HEIGHT as i32 + y,
                         );
-                        let classes = tile.get_tile().unwrap().user_type.clone().unwrap_or_default();
-                        let classes = classes.split(' ').map(|x|(x.to_owned(), ()));
-                        let classes:HashMap<String,()> = classes.collect();
+                        let classes = tile
+                            .get_tile()
+                            .unwrap()
+                            .user_type
+                            .clone()
+                            .unwrap_or_default();
+                        let classes = classes.split(' ').map(|x| (x.to_owned(), ()));
+                        let classes: HashMap<String, ()> = classes.collect();
                         //let mut keys = HashMap::default();
                         if classes.contains_key("player") {
-                          /*  let key = entities.insert(Entity {
+                            /*  let key = entities.insert(Entity {
                                 index:tile.id() as u16,
                                 pos:tile_pos.into(),
                                 is_player:true
                             });
                             keys.insert(key, ());*/
                         }
-                         
-                        grid.insert(tile_pos, Tile {
+
+                        grid.insert(
+                            tile_pos,
+                            Tile {
                            /* index:if classes.contains_key("entity") { 0 } else { tile.id() as u16 },
                             solid:classes.contains_key("solid"),
                             entities:keys*/
-                        });
+                        },
+                        );
                     }
                 }
             }
@@ -60,4 +65,6 @@ async fn main() {
     let mut grid = Grid::default();
     let mut entities = Default::default();
     load_map(&mut grid, &mut entities);
+
+    let server = Server::default() as Server<String>;
 }
