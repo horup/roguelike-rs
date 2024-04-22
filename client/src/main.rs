b95a4a940c5a1ace184b92f4359042a0b13a0733
Søren Alsbjerg Hørup <sah@sah.dk>
2024-04-22 12:58:51 +0200

    wip

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,12 @@
-use std::sync::Mutex;
-use bevy::{prelude::*, render::{settings::{Backends, WgpuSettings}, RenderPlugin}};
+use bevy::{
+    prelude::*,
+    render::{
+        settings::{Backends, WgpuSettings},
+        RenderPlugin,
+    },
+};
 use shared::Message;
+use std::sync::Mutex;
 use uuid::Uuid;
 
 #[derive(Component)]
@@ -8,85 +14,89 @@ pub struct CenterText;
 
 #[derive(Resource)]
 pub struct Player {
-    pub id:Uuid,
-    pub name:String,
-    pub client:Mutex<netcode::client::Client<Message>>
+    pub id: Uuid,
+    pub name: String,
+    pub client: Mutex<netcode::client::Client<Message>>,
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(RenderPlugin {
-            render_creation:bevy::render::settings::RenderCreation::Automatic(WgpuSettings {
-                backends:Some(Backends::VULKAN),
+            render_creation: bevy::render::settings::RenderCreation::Automatic(WgpuSettings {
+                backends: Some(Backends::VULKAN),
                 ..Default::default()
             }),
             ..Default::default()
         }))
         .insert_resource(Player {
-            client:Mutex::new(Default::default()),
-            id:Uuid::new_v4(),
-            name:"Player".to_owned()
+            client: Mutex::new(Default::default()),
+            id: Uuid::new_v4(),
+            name: "Player".to_owned(),
         })
         .add_systems(Startup, setup)
         .add_systems(Update, update)
         .run();
 }
 
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, client:ResMut<Player>) {
+fn setup(
+    mut commands: Commands,
+    _asset_server: Res<AssetServer>,
+    client: ResMut<Player>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     client.client.lock().unwrap().connect("ws://localhost:8080");
-    commands.spawn(Camera2dBundle {
-        projection:OrthographicProjection {
-            near:-1000.0,
-            far:1000.0,
-            scaling_mode:bevy::render::camera::ScalingMode::WindowSize(2.0),
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera3dBundle {
+        camera:Camera {
+            order:-1,
             ..Default::default()
         },
-        ..Default::default()
+        transform: Transform::from_xyz(15.0, 5.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
     });
-
-    for x in 0..100 {
-        commands.spawn((
-            SpriteBundle {
-                texture: asset_server.load("imgs/player.png"),
-                ..default()
-            },
-            Direction::Up,
-        ));
-    }
-   
-
-    commands.spawn(Text2dBundle {
-        text:Text::from_section("", TextStyle {
-            font_size:16.0,
+    commands
+        .spawn(Text2dBundle {
+            text: Text::from_section(
+                "",
+                TextStyle {
+                    font_size: 16.0,
+                    ..Default::default()
+                },
+            ),
             ..Default::default()
-        }),
-        ..Default::default()
-    }).insert(CenterText);
+        })
+        .insert(CenterText);
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane3d::default().mesh().size(20., 20.)),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3)),
+        ..default()
+    });
 }
 
-fn update(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>, player:ResMut<Player>, mut center_text:Query<&mut Text, With<CenterText>>) {
+fn update(
+    player: ResMut<Player>,
+    mut center_text: Query<&mut Text, With<CenterText>>,
+) {
     let mut client = player.client.lock().unwrap();
     for e in client.poll() {
         match e {
             netcode::client::Event::Connecting => {
                 center_text.single_mut().sections[0] = "Connecting...".into();
-            },
+            }
             netcode::client::Event::Connected => {
-                client.send(Message::JoinAsPlayer { id: player.id, name: player.name.clone() });
+                client.send(Message::JoinAsPlayer {
+                    id: player.id,
+                    name: player.name.clone(),
+                });
                 center_text.single_mut().sections[0] = "".into();
-            },
+            }
             netcode::client::Event::Disconnected => {
                 center_text.single_mut().sections[0] = "Lost connection to server!".into();
-            },
-            netcode::client::Event::Message(_) => {
-                
-            },
+            }
+            netcode::client::Event::Message(_) => {}
         }
     }
 }
